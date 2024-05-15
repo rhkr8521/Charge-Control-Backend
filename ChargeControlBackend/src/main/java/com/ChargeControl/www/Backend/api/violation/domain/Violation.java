e5484967e95d752ef22472de973529edf9973dd9
rhkr8521 <rhkr8521@gmail.com>
2024-05-15 09:10:46 +0900

    [fix] 위반내역 발행날짜 누락 수정

--- ChargeControlBackend/src/main/java/com/ChargeControl/www/Backend/api/violation/domain/Violation.java
+++ ChargeControlBackend/src/main/java/com/ChargeControl/www/Backend/api/violation/domain/Violation.java
@@ -1,16 +1,19 @@
 package com.ChargeControl.www.Backend.api.violation.domain;
 
+import com.ChargeControl.www.Backend.api.question.domain.Question;
 import com.ChargeControl.www.Backend.common.entity.BaseTimeEntity;
-
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.List;
 
 @Getter
 @Entity
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class Violation extends BaseTimeEntity{
+public class Violation extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long violationId;
@@ -21,4 +24,7 @@ public class Violation extends BaseTimeEntity{
     private String violationContent;
     private String result;
 
+    @OneToMany(mappedBy = "violation", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private List<Question> questions;
 }
